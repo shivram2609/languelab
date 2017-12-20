91f263f2eab4c2b8f80df437e9a901cde4ae2828
shivram2609 <shivram2609>
2017-12-20 15:21:32 +0530

    media files

--- app/Controller/CoursesController.php
+++ app/Controller/CoursesController.php
@@ -2114,7 +2114,7 @@ function policies($id) {
 					//pr($data);
                    //die;
 					if ($this->CourseQuizQuestion->save($data)) {
-					die("here");
+					//die("here");
 						if ( !empty($questionID) ) {
 							$quistId = $questionID;
 						} else {
@@ -2122,7 +2122,8 @@ function policies($id) {
 						}
 							$this->redirect("/addquizquestion/".$quizID."/".$questionType."/".$quistId);
 					} else {
-						$this->request->data = $data;
+						pr($this->request->data = $data);
+						die;
 					}
 				}
 			}
