13e371688a0050f71f203395ff60b734bd210d53
root <shivam@zestminds.com>
2018-10-04 14:45:59 +0530

    commiting changes for recording

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -155,7 +155,7 @@ class PagesController extends AppController {
 	function test_recording() {
 		
 		if ($this->request->is('ajax')) {
-			pr("here");
+			pr($_FILES);
 		die;
 		}	
 		//~ pr($this->request->data);
